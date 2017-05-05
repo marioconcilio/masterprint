72ea4c46bf928ae2b7a218e4e61ed1cd61f65f16
Mario Concilio <marioconcilio@gmail.com>
2017-05-05 01:53:05 -0300

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = Usuario.find_by(usuario: params[:session][:user].downcase)
+    if user.try(:authenticate, params[:session][:password])
+      log_in user
+      redirect_to estoque_blanquetas_path
+    else
+      flash.now[:danger] = 'Usuário/senha inválidos'
+      render 'new'
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
