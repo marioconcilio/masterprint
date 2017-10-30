14b14fb0c6af39aea0c2cf5d4c4e981ed6612ca4
marioconcilio <marioconcilio@gmail.com>
2017-10-30 17:01:25 -0200

    fixing merge

--- spec/factories/tiktak/calendarios.rb
+++ spec/factories/tiktak/calendarios.rb
@@ -1,26 +1,17 @@
 FactoryGirl.define do
   factory :tiktak_calendario, :class => Tiktak::Calendario do
     pagina { Faker::Lorem.word }
-<<<<<<< HEAD
     referencia { Faker::Lorem.word }
-=======
-    referencia { Faker::Lorem.sentence }
->>>>>>> c7b0b308ab5189b7fb0c8c68d116183e37300186
     descricao { Faker::Lorem.sentence }
     preco { Faker::Number.decimal(3, 2) }
     un { Faker::Lorem.word }
 
     tipo do
-<<<<<<< HEAD
       Tiktak::Calendario.tipos.keys.sample
     end
   end
 
   factory :invalid_tiktak_calendario, parent: :tiktak_calendario do
     pagina nil
-=======
-      Tiktak::Calendario.tipos.values.sample
-    end
->>>>>>> c7b0b308ab5189b7fb0c8c68d116183e37300186
   end
 end
